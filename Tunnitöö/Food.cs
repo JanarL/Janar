7cdc39e85c68178896d66d979710f3cf30afc0b7
janar liiv <janar.liiv@hotmail.com>
2019-09-24 22:23:31 +0300

    Tunnitöö

---
+++ Tunnitöö/Food.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tunnitöö
+{
+    class Food
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Food(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+    }
+}
